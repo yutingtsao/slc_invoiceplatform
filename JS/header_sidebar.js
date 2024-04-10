cfb7752ac481ef4a0f98bc6dbe087bc625436cc5
yutingtsao <tsao850924@gmail.com>
2024-04-10 14:54:19 +0800

    Complete withour sidebar_toggle

---
+++ JS/header_sidebar.js
@@ -0,0 +1,17 @@
+
+$(function(){
+    $('.invoice_setting').click(function(){
+        $('.dropdown_block1').toggle('normal')
+    })
+
+    $('.serial_setting').click(function(){
+        $('.dropdown_block2').toggle('normal')
+    })
+
+
+    // $('.toggle_icon').click(function(){
+    //     $('aside').toggle('normal')
+    // })
+
+    // console(toggle_icon)
+})
