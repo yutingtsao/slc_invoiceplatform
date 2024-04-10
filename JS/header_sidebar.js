378dfbf1e27e8e5b3137f9c75e37ac89aa40e75d
yutingtsao <tsao850924@gmail.com>
2024-04-10 16:32:24 +0800

    update notification(still editing)

--- JS/header_sidebar.js
+++ JS/header_sidebar.js
@@ -1,5 +1,5 @@
 
-$(function(){
+$(document).ready(function(){
     $('.invoice_setting').click(function(){
         $('.dropdown_block1').toggle('normal')
     })
@@ -8,10 +8,37 @@ $(function(){
         $('.dropdown_block2').toggle('normal')
     })
 
-
     // $('.toggle_icon').click(function(){
     //     $('aside').toggle('normal')
     // })
 
     // console(toggle_icon)
 })
+
+$(document).ready(function(){
+    $('.fa-solid fa-xmark').click(function(){
+        $('.notification').animate({right:"0px"})
+        $('.mask_noti').css('display','block')
+        $('.mask_noti').animate({right:"-433px"})
+    })
+
+    // $('.right_side').click(function(){
+    //     $('.notification').animate({right:"-433px"})
+    // })
+
+    $('.noto_xmark').click(function(){
+        $('.mask_noti').css('display','none')
+        window.history.back()
+    })
+
+    $('.mask_noti').click(function(){
+        $(this).removeAttr('style')
+    })
+    
+    $('.notification').click(function(e){
+        e.stopPropagation()
+    })
+
+    
+})
+
