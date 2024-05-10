8c1e4198e9ea018044b78c4aed29eb36bf577382
yutingtsao <tsao850924@gmail.com>
2024-05-10 12:00:23 +0800

    matching function result dialog completed

--- JS/Matching-function.js
+++ JS/Matching-function.js
@@ -11,6 +11,32 @@ $(document).ready(function(){
         });
     }
 
+    // 配號結果隨機顯示
+    $('.confirm_matching-insidecheck_bt').click(function() {
+        // 這裡需要添加你的業務邏輯來判斷應該顯示哪個模態框
+        // 以下是一個簡單的例子，隨機選擇一個結果
+        var result = Math.floor(Math.random() * 4); // 生成一個 0 到 3 的隨機數
+
+        switch(result) {
+            case 0:
+                $('#matching-result-success_popup').modal('show');
+                break;
+            case 1:
+                $('#matching-result-alert_popup').modal('show');
+                break;
+            case 2:
+                $('#matching-result-fail_popup').modal('show');
+                break;
+            case 3:
+                $('#matching-result-done_popup').modal('show');
+                break;
+        }
+    });
+
+    // 為所有模態框中的確認按鈕添加事件處理器，關閉模態框
+    $('.confirm_matching_bt').click(function() {
+        $('.modal').modal('hide');
+    });
 
     
 })
